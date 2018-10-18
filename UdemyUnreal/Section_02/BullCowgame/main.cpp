1a0dcaffb87cc7443461821fd24321b978eb4646
Suthi <sseneth@my.uri.edu>
2018-10-18 00:51:59 -0400

    Added loop

--- UdemyUnreal/Section_02/BullCowgame/main.cpp
+++ UdemyUnreal/Section_02/BullCowgame/main.cpp
@@ -7,14 +7,18 @@ using namespace std;
 void PrintIntro();
 string GetGuessAndPrintBack();
 
+constexpr int WORD_LENGTH = 5;
+
 // the entry point for our application
 int main(){
 
 	PrintIntro();
 
-	GetGuessAndPrintBack();
-
-	cout << endl;
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int i = 1; i <= NUMBER_OF_TURNS; i++) {
+		GetGuessAndPrintBack();
+		cout << endl;
+	}
 
 	return 0;
 } 
@@ -22,7 +26,6 @@ int main(){
 void PrintIntro(){
 
 	// introduce the game
-	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
@@ -32,6 +35,7 @@ void PrintIntro(){
 
 string GetGuessAndPrintBack() {
 
+	// get guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
